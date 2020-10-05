0350f12a36f5432701ffbef6e013d4b7dbd4d9dc
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-10-05 20:32:07 +0530

    added most basic of codes

---
+++ src/pages/aboutpage/aboutpage.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './aboutpage.styles.css';
+
+const Aboutpage= () => (
+    <div>About Page</div>
+)
+
+export default Aboutpage;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.css';
+
+const Homepage= () => (
+    <h2>
+        Homepage
+    </h2>
+);
+
+export default Homepage;
