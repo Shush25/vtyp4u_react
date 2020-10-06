0f9f89093d2258de3e0b757fb464b188ac065652
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-10-07 02:13:28 +0530

    added styling to the cards and updated aboutpage.data.js

--- src/pages/aboutpage/aboutpage.data.js
+++ src/pages/aboutpage/aboutpage.data.js
@@ -1,21 +1,33 @@
-const team =[
+const team_members =[
     {
         id: 1,
         name:'Shushant Kumar Singh',
-        email: 'shushantkumar.singh2019@vitstudent.ac.in',
-        imageUrl: ''
+        Job:'React Developer',
+        email: 'mailto:shushantkumar.singh2019@vitstudent.ac.in',
+        github: 'https://github.com/Shush25',
+        linkdin: 'https://www.linkedin.com/in/shushant-kumar-singh-bb016b1b6/',
+        insta: '',
+        imageUrl: 'https://lh3.googleusercontent.com/-DdxNGQg4hPbEmzbCmR08sUX9cuPu1NPQ1wsXmnWo4mt-yPL-wsP8zVx2S9_xDc_ezYdZpX8RmkYdWmSiJUmOt3ZDjFyLyrF1cduSH14E3LmDal786A5Y6PzUgI7xrn6sv7yv8PWjA'
     },
     {
         id: 2,
         name: 'Apoorv Yadav',
-        email: 'apoorv.yadav2019@vitstudent.ac.in',
-        imageUrl: ''
+        Job: 'Front-End Developer',
+        email: 'mailto:apoorv.yadav2019@vitstudent.ac.in',
+        github: 'https://github.com/Apoorv-17',
+        linkdin: 'https://www.linkedin.com/in/apoorv-yadav-1a54571b7/',
+        insta: '',
+        imageUrl: 'https://lh3.googleusercontent.com/ARVj1C0lnGyiwERJZWQXMfzG-gBbQDPm3zzH9qw1RNXvhMVyp31hzFrb1IpLJtBBdouGffMhu2r_6gUeQzwivrwUpdXV0wxgC1cuZmPFB-GQeZwouXJKIMJ8KsL2NmYINwb2yjDufw'
     },
     {
         id: 3,
         name: 'Tanmay Mahajan',
-        email: 'tanmay.mahajan2019@vitstudent.ac.in',
-        imageUrl: ''
+        Job: 'HTML,CSS Designeer',
+        github: '',
+        linkdin: 'https://www.linkedin.com/in/tanmay-mahajan-6ba24b176/',
+        insta: '',
+        email: 'mailto:tanmay.mahajan2019@vitstudent.ac.in',
+        imageUrl: 'https://www.w3schools.com/howto/img_avatar.png'
     }
 ]
-export default team;
+export default team_members;
