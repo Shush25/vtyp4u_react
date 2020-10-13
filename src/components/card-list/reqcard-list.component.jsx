92745364f2207ce0d70d244372d4b58651a8c35d
Apoorv <apoorv.ydv17@gmail.com>
2020-10-13 20:30:42 +0530

    debugged reqcardlist.css

--- src/components/card-list/reqcard-list.component.jsx
+++ src/components/card-list/reqcard-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Cardr } from '../card/reqcard.component';
 import './reqcard-list.styles.css';
 export const CardrList= props =>(
-    <div className='reqcard-list'>
+    <div className='cardr-list'>
         {props.team.map(member=>(
             <Cardr key={member.id} member={member}/>
         ))}
