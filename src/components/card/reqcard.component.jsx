e3e2822ee0d73c1fc0a1783f469d5e46c4fedb9a
Apoorv <apoorv.ydv17@gmail.com>
2020-10-21 01:42:44 +0530

    Adding final touch to request page

--- src/components/card/reqcard.component.jsx
+++ src/components/card/reqcard.component.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
+import { Icon } from '@iconify/react';
+import coinsLine from '@iconify/icons-ri/coins-line';
+
 import './reqcard.styles.css';
 
-export const Cardr = props =>(
+export const Cardr = props =>( 
     <div className = "card-container1">
         <div className="row1">
-            <div className="column">
-                <h2><b> {props.member.name} </b></h2>
+            <div className="name">
+                <p><b> {props.member.name} </b></p>
             </div>
             <div className="column2" align="right">
-                <button value="Take Task" className="btn"><b>Take Task</b></button>
+                    <button value="Take Task" className="btn"><b>Take Task</b></button>   
             </div>           
         </div>
-        <h3><b>Deadline:</b> {props.member.deadline} </h3>
-        <h3><b>Coins:</b> {props.member.coins} </h3>
-        <p> {props.member.note} </p>
+        <div className="row2">
+            <div className="author">
+                <p> {props.member.author} </p>
+            </div>
+            <div className="deadline">
+                <p><b>Deadline:</b> {props.member.deadline} </p>
+            </div>
+            <div className="coins">
+                <p>
+                    <b>Coins:</b> {props.member.coins}   <Icon icon={coinsLine} color="gold" width="30px" height="25px" />
+                </p>
+            </div>
+            <div className="note">
+                <p> {props.member.note} </p>  
+            </div>     
+        </div>
+
     </div>
 )
--- src/pages/requestpage/requestpage.data.js
+++ src/pages/requestpage/requestpage.data.js
@@ -1,25 +1,35 @@
 const user_members =[
     {
         id: 1,
-        name:'Raghav Gutgutiya',
+        name:'Software Engineering',
+        author:'Ranjita Jaiswal',
         deadline: '19-10-2020',
         coins: '250',
         note: 'Complete all the work on time and make sure there is appropriate margin is given and there is no grammatical mistake done.'
     },
     {
         id: 2,
-        name:'Ashish Das',
+        name:'Art of Deception',
+        author:'JonSnow',
         deadline: '30-10-2020',
         coins: '300',
         note: 'The task should be done within the time limit and that the spacing should be given same as in the notes.'
     },
     {
         id: 3,
-        name:'Divyanu Bhaeti',
+        name:'Engineering Mathematics',
+        author:'B.S. Grewal',
         deadline: '25-11-2020',
         coins: '200',
         note: 'The notes should be the exact copy of the handwritten notes and that for the diagram I will be giving separate pictures for that to be pasted.'
+    },
+    {
+        id: 4,
+        name: 'Bussiness Management',
+        author:'Yash Yadav',
+        deadline: '17-11-2020',
+        coins: '260',
+        note: 'The notes should be containing all the important points in it and that the indentation given should same as that in the handwritten notes.'
     }
-
 ]
 export default user_members;
