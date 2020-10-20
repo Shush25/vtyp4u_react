26b824f578884eb7edfd24fed36e4309533b5837
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-10-21 03:41:40 +0530

    added animations too Hurraygit add -A!

--- src/pages/requestpage/requestpage.component.jsx
+++ src/pages/requestpage/requestpage.component.jsx
@@ -18,12 +18,14 @@ class Requestpage extends React.Component{
         return(
             <div className='aboutpage'>
                 <Header Header_image={Header_image}>
-                <h1 style={{color :'white',fontSize: '50px'}}>
-                    Hello, Want to Earn Coins,</h1>
-                    <p style={{fontSize: '40px'}}>Start Taking Task For Others.</p><br></br>
-                    <div align="right"><Link className="button" to="/about">Type for Me</Link></div>
+                <h1 style={{color :'white',fontSize: '50px',textAlign:'center'}} data-aos="fade-up" data-aos-duration="700">
+                    Want to Earn Coins,</h1>
+                    <p style={{fontSize: '40px',textAlign:'center'}} data-aos="fade-up" data-aos-duration="900">Start Taking Task For Others.</p><br></br>
+                    <div align="right" data-aos="fade-up" data-aos-duration="1100"><Link className="button" to="/about">Get Started</Link></div>
                 </Header>
-                <br></br><br></br><br></br><br></br><br></br>
+                <div className="main-heading">
+                    <h1>Available Requests</h1>
+                </div>
                 <CardrList team={team}/>
             </div>
         )
